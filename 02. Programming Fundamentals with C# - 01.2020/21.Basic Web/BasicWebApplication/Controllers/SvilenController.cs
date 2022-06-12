e9fc32e2c91ffd53c4e5ab6921e82f227c5d7f09
Svilen Stoev <svilen.d.stoev@abv.bg>
2022-06-12 19:54:04 +0300

    21.Basic Web

---
+++ 02. Programming Fundamentals with C# - 01.2020/21.Basic Web/BasicWebApplication/Controllers/SvilenController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BasicWebApplication.Controllers
+{
+    public class SvilenController : Controller
+    {
+        public IActionResult Hello(int id = 10)
+        {
+            int number = 0;
+            List<string> result = new List<string>();
+
+            for (int i = 0; i < id; i++)
+            {
+                number++;
+                result.Add(number.ToString());
+            }
+
+            ViewBag.Num = string.Join(" ", result);
+            return View();
+        }
+    }
+}
