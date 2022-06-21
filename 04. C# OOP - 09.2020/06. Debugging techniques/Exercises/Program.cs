91a541ff4444265c550cbed6f237e6595485208e
Svilen Stoev <svilen.d.stoev@abv.bg>
2022-06-21 13:19:09 +0300

    07. SOLID

---
+++ 04. C# OOP - 09.2020/06. Debugging techniques/Exercises/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exercises
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //    string s = "";
+
+            //    for (int i = 0; i < 1000; i++)
+            //    {
+            //        s += i;
+            //    }
+
+            //;
+
+            //string input = Console.ReadLine();
+
+            //double[] nums = input
+            //.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            //.Select(int.Parse)
+            //.Where(n => n % 2 == 0)
+            //.Select(n => Math.Pow(n, 2) + 1)
+            //.ToArray();
+
+            ;
+
+            int a = 5;
+
+            int b = 10;
+
+            Console.WriteLine(a + b);
+        }
+    }
+}
---
+++ 04. C# OOP - 09.2020/07. SOLID/Excercises/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Excercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
